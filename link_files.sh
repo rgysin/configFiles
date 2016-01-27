c11468fbb75cd52c8699f69077d52b1320b23df3
Ryan Gysin <rgysin@umich.edu>
2016-01-27 16:48:44 -0500

    Removed some print statements, less output is better

--- link_files.sh
+++ link_files.sh
@@ -6,11 +6,7 @@ for file in ${FILES}
 do
     if [ `echo ${IGNORE} | grep -c ${file}` -eq 0 ];
     then
-        echo "Removing ~/${file}"
         rm -rf ~/${file}
-        echo "Creating link to ~/${PWD}/${file} in its place"
         ln -s ${PWD}/${file} ~/${file}
-    else
-        echo "Ignoring ${file}"
     fi
 done
